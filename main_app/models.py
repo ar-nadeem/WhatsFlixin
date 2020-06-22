934405eaf452d0f643578ffcf9d184aea7d201ff
AbdulRahman Nadeem <ar-nadeem@outlook.com>
2020-06-23 02:40:13 +0500

    Increased Models Lenght

--- main_app/models.py
+++ main_app/models.py
@@ -5,9 +5,9 @@ class PopularMovie(models.Model):
     poster_link = models.CharField(max_length=500)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    rank = models.CharField(max_length=5)
-    rating = models.CharField(max_length=25)
-    netflix_url = models.CharField(max_length=120, default="#")
+    rank = models.CharField(max_length=100)
+    rating = models.CharField(max_length=100)
+    netflix_url = models.CharField(max_length=500, default="#")
 
     def __str__(self):
         return '{} - {}'.format(self.rank, self.title)
@@ -19,9 +19,9 @@ class TopMovie(models.Model):
     poster_link = models.CharField(max_length=500)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
-    rank = models.CharField(max_length=5)
-    rating = models.CharField(max_length=25)
-    netflix_url = models.CharField(max_length=120, default="#")
+    rank = models.CharField(max_length=100)
+    rating = models.CharField(max_length=100)
+    netflix_url = models.CharField(max_length=500, default="#")
 
     def __str__(self):
         return '{} - {}'.format(self.rank, self.title)
