231c2c40ab4228e767ddfc265d587aeb424f219e
AbdulRahman Nadeem <ar-nadeem@outlook.com>
2020-07-27 14:57:48 +0500

    Improved UI  Added redirect for previous url

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,8 +4,11 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='movie/popular/')),
+    path('popular-movie', RedirectView.as_view(url='movie/popular/')),
+
     path('movie', RedirectView.as_view(url='movie/popular/')),
     path('tv', RedirectView.as_view(url='tv/popular/')),
+
 
     path('movie/popular/', views.imdbPopMovieView, name='imdbPopMovieView'),
     path('movie/top-rated/', views.imdbTopMovieView, name='imdbTopMovieView'),
