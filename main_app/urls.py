19583e4b8037b3f4a3da336ff6b06b2dcb27f5a0
AbdulRahman Nadeem <ar-nadeem@outlook.com>
2020-07-27 14:19:12 +0500

    Massive UI UX improvement

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,17 +3,15 @@ from . import views
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='popular-movie/')),
+    path('', RedirectView.as_view(url='movie/popular/')),
+    path('movie', RedirectView.as_view(url='movie/popular/')),
+    path('tv', RedirectView.as_view(url='tv/popular/')),
 
-    path('popular-movie/', views.imdbPopMovieView, name='imdbPopMovieView'),
+    path('movie/popular/', views.imdbPopMovieView, name='imdbPopMovieView'),
+    path('movie/top-rated/', views.imdbTopMovieView, name='imdbTopMovieView'),
 
-
-    path('top-rated-movie/', views.imdbTopMovieView, name='imdbTopMovieView'),
-
-    path('popular-tv/', views.imdbPopTvView, name='imdbPopTvView'),
-
-
-    path('top-rated-tv/', views.imdbTopTvView, name='imdbTopTvView'),
+    path('tv/popular/', views.imdbPopTvView, name='imdbPopTvView'),
+    path('tv/top-rated/', views.imdbTopTvView, name='imdbTopTvView'),
 
 
 
