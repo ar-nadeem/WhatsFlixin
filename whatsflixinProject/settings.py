58cd74ccebc8e75fdef40ed102ecc47899c7e29b
AbdulRahman Nadeem <ar-nadeem@outlook.com>
2020-08-02 15:51:03 +0500

    geoip

--- whatsflixinProject/settings.py
+++ whatsflixinProject/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 ###################### FOR HEROKU NEEDS TO BE TURNED ON ##################################
 SECURE_SSL_REDIRECT = True
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -144,6 +144,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 GEOIP_PATH = os.path.join(BASE_DIR, 'geoip')
-
+GEOIP_CITY=os.environ['GeoLite2-City.mmdb']
+GEOIP_COUNTRY=os.environ['GeoLite2-Country.mmdb']
 
 django_heroku.settings(locals())
